b8736b40148d204ff8745766050fee66fa85f6b7
Jean-Alix <jean-alix.david@laposte.net>
2014-04-03 15:30:11 +0200

    script exo 421

---
+++ Exercice1/matlab/exercice421.m
@@ -0,0 +1,55 @@
+clear all
+close all
+
+s = tf('s');
+G = 20/((s + 1)*((s/20)^2+s/20+1));
+Gd = 10/(s+1);
+
+omegaC = 3/0.2; %tr = 0.2s
+phi0 = 60;      %D = 10%
+
+bode(Gd)
+
+% 
+% %--------------------
+% % Phase-lag action
+% %--------------------
+% gamma = 0;
+% Ti = 10;
+% 
+% Flag = (Ti*s+1)/(Ti*s+gamma);
+% 
+% %--------------------
+% % Phase-lead action
+% %--------------------
+% 
+% [m,p] = bode(G*Flag,omegaC);
+% p = p - 360;
+% [Gm,Pm,wp,wc] = margin(G*Flag);
+% 
+% phim = phi0 - (180+p);
+% a = (1 - sin(phim*pi/180))/(1 + sin(phim*pi/180));
+% T = 1 / (omegaC * sqrt(a));
+% K = sqrt(a)/m;
+% 
+% Flead = K*(1+T*s)/(1 + a*T*s);
+% % margin(F)
+% % margin(F*G);
+% 
+% Gc = G*Flag*Flead;
+% 
+% figure
+% hold on
+% step(G);
+% step(feedback(Gc,1));
+% 
+% figure
+% hold on
+% margin(G*Flag)
+% margin(Flag*Flead)
+% margin(Gc)
+% 
+% S = stepinfo(feedback(Gc,1));
+% 
+% figure
+% bode(feedback(Gc,1));
