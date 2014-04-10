91c395350647989ca199d400a915e48b4a3d4833
Jean-Alix <jean-alix.david@laposte.net>
2014-04-10 14:24:10 +0200

    script questions 311 312 313

--- Exercice2/matlab/exercice2.m
+++ Exercice2/matlab/exercice2.m
@@ -8,10 +8,28 @@ sysnmp = nonminphase;
 [Amp,Bmp,Cmp,Dmp] = ssdata(sysmp);
 [Anmp,Bnmp,Cnmp,Dnmp] = ssdata(sysnmp);
 
-Gmp = tf(ssdata(sysmp));
+Gmp = tf(sysmp);
+Gnmp = tf(sysnmp);
 
+Zmp = cell(2);
+Znmp = cell(2);
+Pmp = cell(2);
+Pnmp = cell(2);
+for i=1:2
+    for j=1:2
+        Zmp{i,j} = zero(Gmp(i,j));
+        Znmp{i,j} = zero(Gnmp(i,j));
+        Pmp{i,j} = pole(Gmp(i,j));
+        Pnmp{i,j} = pole(Gnmp(i,j));
+    end
+end
 
+zmp = tzero(Gmp);
+znmp = tzero(Gnmp);
 
-% sigma(sysmp)
-% hold on
-% sigma(sysnmp)
+pmp = pole(Gmp);
+pnmp = pole(Gnmp);
+
+sigma(Gmp)
+hold on
+sigma(Gnmp)
