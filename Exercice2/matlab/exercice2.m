02d175a5b4955c79db9f2d74fd94eb6d2d8fef1b
Jean-Alix <jean-alix.david@laposte.net>
2014-04-10 18:30:09 +0200

    correcteur exo2

--- Exercice2/matlab/exercice2.m
+++ Exercice2/matlab/exercice2.m
@@ -44,6 +44,47 @@ RGAnmp = Gnmp.*(Gnmp^-1)';
 RGA0mp =  freqresp(RGAmp,0);
 RGA0nmp =  freqresp(RGAnmp,0);
 
-step(Gmp);
-figure
-step(Gnmp);
+% step(Gmp);
+% figure
+% step(Gnmp);
+
+phim = pi/3;
+wcmp = 0.1;
+wcnmp = 0.02;
+syms Ti
+
+
+T1mp = double(solve(phase(freqresp(Gmp(1,1),wcmp)) + atan(wcmp*Ti) - phim == -pi/2,Ti));
+K1mp = 1/(abs(freqresp(Gmp(1,1),wcmp))*abs(freqresp(tf([T1mp 1],[T1mp 0]),wcmp)));
+F1mp = tf([K1mp*T1mp K1mp],[T1mp 0]);
+
+T2mp = double(solve(phase(freqresp(Gmp(2,2),wcmp)) + atan(wcmp*Ti) - phim == -pi/2,Ti));
+K2mp = 1/(abs(freqresp(Gmp(2,2),wcmp))*abs(freqresp(tf([T2mp 1],[T2mp 0]),wcmp)));
+F2mp = tf([K2mp*T2mp K2mp],[T2mp 0]);
+
+
+T1nmp = double(solve(phase(freqresp(Gnmp(1,2),wcnmp)) + atan(wcnmp*Ti) - phim == -pi/2,Ti));
+K1nmp = 1/(abs(freqresp(Gnmp(1,2),wcnmp))*abs(freqresp(tf([T1nmp 1],[T1nmp 0]),wcnmp)));
+F1nmp = tf([K1nmp*T1nmp K1nmp],[T1nmp 0]);
+
+T2nmp = double(solve(phase(freqresp(Gnmp(2,1),wcnmp)) + atan(wcnmp*Ti) - phim == -pi/2,Ti));
+K2nmp = 1/(abs(freqresp(Gnmp(2,1),wcnmp))*abs(freqresp(tf([T2nmp 1],[T2nmp 0]),wcnmp)));
+F2nmp = tf([K2nmp*T2nmp K2nmp],[T2nmp 0]);
+
+
+
+F = [F1mp,0;0,F2mp];
+G = Gmp;
+
+% F = [0 F1nmp;F2nmp 0];
+% G = Gnmp;
+
+L = F*G; 
+
+I = tf(eye(size(G*F)));
+S = (I+G*F)^-1;
+T = S*G*F;
+
+sigma(S);
+hold on
+sigma(T);
