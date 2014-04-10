1a99acb85a1b533ec126d95d89b17f53bbdc396d
Jean-Alix <jean-alix.david@laposte.net>
2014-04-10 15:02:52 +0200

    maj code 2latex

--- Exercice2/matlab/exercice2.m
+++ Exercice2/matlab/exercice2.m
@@ -2,7 +2,7 @@ close all
 clear all
 clc
 
-syms s
+
 
 sysmp = minphase;
 sysnmp = nonminphase;
--- Exercice2/matlab/tf2latex.m
+++ Exercice2/matlab/tf2latex.m
@@ -3,9 +3,10 @@ function [ code ] = tf2latex( t )
 %   Detailed explanation goes here
 
 syms s
-
+digits(2);
 [num,den] = tfdata(t);
 t_sym = poly2sym(cell2mat(num),s)/poly2sym(cell2mat(den),s);
-code = latex(t_sym);
+code = latex(vpa(t_sym,5));
+
 end
 
