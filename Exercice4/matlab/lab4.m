b9667b63d23404ee1ab1c219c21e75c04c2c2a68
Jean-Alix <jean-alix.david@laposte.net>
2014-05-09 15:21:52 +0200

    script lab4

---
+++ Exercice4/matlab/lab4.m
@@ -0,0 +1,45 @@
+close all
+clear all
+clc
+
+s = tf('s');
+sysmp = minphase;
+sysnmp = nonminphase;
+Gmp = tf(sysmp);
+Gnmp = tf(sysnmp);
+
+RGAmp = Gmp.*(Gmp^-1)';
+RGAnmp = Gnmp.*(Gnmp^-1)';
+
+wc_mp = 0.1;
+wc_nmp = 0.02;
+phim = pi/3;
+
+w11 = 1;
+w22 = 1;
+w12 = minreal(-Gmp(1,2)/Gmp(1,1));
+w21 = minreal(-Gmp(2,1)/Gmp(2,2));
+
+W1 = [[w11 w12];[w21 w22]];
+Gt_mp = Gmp*W1;
+bode(Gt_mp)
+grid
+
+Ti_mp = 1/(2*wc_mp);
+
+f11 = (1+Ti_mp*s)/(Ti_mp*s);
+f22 = (1+Ti_mp*s)/(Ti_mp*s);
+
+K11_mp = 10^(3.4/20);
+K22_mp = 10^(4.53/20);
+
+f11_mp = K11_mp*f11;
+f22_mp = K22_mp*f22;
+
+Ft_mp = [[f11_mp 0];[0 f22_mp]];
+% bode(Ft_mp*Gt_mp)
+
+F_mp = W1*Ft_mp;
+% figure
+% bode(F_mp*Gmp)
+% grid
--- Project/matlab/lab1.m
+++ Project/matlab/lab1.m
@@ -4,20 +4,20 @@ clc
 
 % Définition des constantes
 
-A = 15.52e-4; %15.52 cm²
+A = 15.52; %15.52 cm²
 gamma_mp = 0.625;
 gamma_nmp = 0.375;
-kc = 0.2e-2; %0.2 V/cm
-g = 9.81;
+kc = 0.2; %0.2 V/cm
+g = 981;
 
 % Systeme
 
-k1 = 10;
-k2 = 1;
-a1 = 1;
-a2 = 1;
-a3 = 1;
-a4 = 1;
+k1 = 8.95;
+k2 = 12.6;
+a1 = sym('a1');
+a2 = sym('a2');
+a3 = sym('a3');
+a4 = sym('a4');
 HU = [[gamma_mp*k1/(a1*sqrt(2*g))     (1-gamma_mp)*k2/(a1*sqrt(2*g))];
      [(1-gamma_mp)*k1/(a2*sqrt(2*g))  gamma_mp*k2/(a2*sqrt(2*g))];
      [0                               ((1-gamma_mp)*k2/a3)/sqrt(2*g)];
@@ -31,25 +31,32 @@ Amp = [[-1/T1 0 1/T3 0];[0 -1/T2 0 1/T4];[0 0 -1/T3 0];[0 0 0 -1/T4]];
 
 % Mesures
 
-u1 = [7.5; 0];
-u2 = [0; 7.5];
-m1 = (HU*[7.5; 0]).^2;
-m2 = (HU*[0; 7.5]).^2;
-h1 = [m1(1); m2(1)];
-h2 = [m1(2); m2(2)];
-h3 = [];
-h4 = [];
+u1 = 7.5;
+u2 = 7.5;
+m1 = (HU*[7.5; 7.5]).^2;
+h1 = 2.9;
+h2 = 12.1;
+h3 = 0.5;
+h4 = 4.4;
+h = [h1;h2;h3;h4];
+S = solve(m1==h);
 
-% Estimation des inconnues
 
-M1 = u1*sqrt(h1)';
-M1t = M1';
-M2 = u2*sqrt(h1)';
-M2t = M2';
-
-M = [gamma_mp*(M1(:)-M1t(:)),(1-gamma_mp)*(M2(:)-M2t(:))];
-
-[U, S, V] = svd(M);
-
-k = V(:,end);
+a1_m =  double(S.a1(1));
+a2_m =  double(S.a2(1));
+a3_m =  double(S.a3(1));
+a4_m =  double(S.a4(1));
+a_m = [a1_m;a2_m;a3_m;a4_m]
+% % Estimation des inconnues
+% 
+% M1 = u1*sqrt(h1)';
+% M1t = M1';
+% M2 = u2*sqrt(h1)';
+% M2t = M2';
+% 
+% M = [gamma_mp*(M1(:)-M1t(:)),(1-gamma_mp)*(M2(:)-M2t(:))];
+% 
+% [U, S, V] = svd(M);
+% 
+% k = V(:,end);
 
