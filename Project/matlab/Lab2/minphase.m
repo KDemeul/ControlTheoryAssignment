df5a8c10d487a6da8d1d50327b01776bdc904774
unknown <pouech@KTH-5096.ug.kth.se>
2014-05-16 15:54:23 +0200

    reg with new values

--- Project/matlab/Lab2/minphase.m
+++ Project/matlab/Lab2/minphase.m
@@ -25,10 +25,10 @@ k1 = 4.0674;     % i cm^3/(Vs)
 k2 = 3.7167;     % i cm^3/(Vs)
 
 % Outlet areas
-a1 = 0.1726;   % i cm^2
-a2 = 0.1479;   % i cm^2
-a3 = 0.0856;  % i cm^2
-a4 = 0.0728;  % i cm^2
+a1 = 0.2234;   % i cm^2
+a2 = 0.1567;   % i cm^2
+a3 = 0.0840;  % i cm^2
+a4 = 0.0817;  % i cm^2
 
 % Valve settings
 gam1 = 0.625;
@@ -40,10 +40,10 @@ HU = [[gam1*k1/(a1*sqrt(2*g))     (1-gam1)*k2/(a1*sqrt(2*g))];
      [((1-gam1)*k1/a4)/sqrt(2*g)  0]];
 h = (HU*[u10; u20]).^2;
 
-h10 = h(1);
-h20 = h(2);
-h30 = h(3);
-h40 = h(4);
+h10 = h(1)
+h20 = h(2)
+h30 = h(3)
+h40 = h(4)
 
 % Compute state-space model for linearized model
 sys = linmodel(a1,a2,a3,a4,h10,h20,h30,h40,u10,u20,k1,k2,gam1,gam2);
